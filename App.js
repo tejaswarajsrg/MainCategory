d925d61d7884c1c1db423959a5e7c548601d0546
tejaswarajsrg <117801940+tejaswarajsrg@users.noreply.github.com>
2025-02-18 10:53:25 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import HomePage from "./pages/HomePage"; // Ensure HomePage.jsx exists
+import "src\assests\styles.css"; // Ensure styles are correctly linked
+
+function App() {
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
+}
+
+export default App;
